e360feb2ae54f10b8822b10b8567a912a5ac9657
Egor Dechenko <gog.992@gmail.com>
2023-08-01 10:54:47 +0300

    SRP 1-2

--- src/srp1/figure/Square.java
+++ src/srp1/figure/Square.java
@@ -22,33 +22,13 @@ public class Square {
         points.add(point4);
     }
 
+    public int getSide() {
+        return side;
+    }
+
     public List<Point> getPoints() {
         return points;
     }
 
-    public int getArea() {
-        return side * side;
-    }
 
-    public void draw() {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < side; i++) {
-            builder.append("* ");
-        }
-        System.out.println(builder);
-        for (int i = 0; i < side; i++) {
-            if (i < side - 2) {
-                System.out.print("*");
-            } else {
-                continue;
-            }
-
-            for (int j = 1; j < side - 1; j++) {
-                System.out.print(" *");
-            }
-            System.out.println(" *");
-        }
-
-        System.out.println(builder);
-    }
 }
---
+++ src/srp1/figure/SquareAreaCalculator.java
@@ -0,0 +1,14 @@
+package srp1.figure;
+
+public class SquareAreaCalculator {
+    private Square square;
+
+    public SquareAreaCalculator(Square square) {
+        this.square = square;
+    }
+
+    public int getArea() {
+        return square.getSide() * square.getSide();
+    }
+
+}
